c18670cf96d1166116bb92fca750185a351d0b96
amalsyahmi <amalsyahmi97@gmail.com>
2022-05-29 12:15:12 +0800

    Fix ui

--- components/blog-card.tsx
+++ components/blog-card.tsx
@@ -1,20 +1,14 @@
 import Link from "next/link";
 // import Image from "next/image";
-import styles from "../styles/BlogCard.module.css"
+import styles from "../styles/BlogCard.module.css";
 
-const BlogCard = ({
-    title,
-    author,
-    coverPhoto,
-    datePublished,
-    slug
-}: any) => {
-    return(
-        <div className={styles.card}>
-            <Link href={'/posts/' + slug}>
-                <div className={styles.imgContainer}>
-                    <img src={coverPhoto.url} alt="" />
-                    {/* <Image
+const BlogCard = ({ title, author, coverPhoto, datePublished, slug }: any) => {
+  return (
+    <Link href={"/posts/" + slug}>
+      <div className={styles.card}>
+        <div className={styles.imgContainer}>
+          <img src={coverPhoto.url} alt="" />
+          {/* <Image
                         
                         src={coverPhoto.url}
                         alt="Cover photo"
@@ -26,22 +20,22 @@ const BlogCard = ({
                         // placeholder="blur" // Optional blur-up while loading
                         layout="fill"
                     /> */}
-                </div>
-            </Link>
-            <div className={styles.text}>
-                <h2>{title}</h2>
-                <div className={styles.details}>
-                    <div className={styles.author}>
-                        <img src={author.avatar.url} alt="" />
-                        <h3>{author.username}</h3>
-                    </div>
-                    <div className={styles.date}>
-                        <h3>{datePublished}</h3>
-                    </div>
-                </div>
-            </div>
         </div>
-    );
-}
+        <div className={styles.text}>
+          <h2>{title}</h2>
+          <div className={styles.details}>
+            <div className={styles.author}>
+              <img src={author.avatar.url} alt="" />
+              <h3>{author.username}</h3>
+            </div>
+            <div className={styles.date}>
+              <h3>{datePublished}</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
 
 export default BlogCard;
